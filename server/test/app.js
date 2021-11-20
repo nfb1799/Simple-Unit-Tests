599f3c916b48e4d34c75961ac7fc094e2624912b
nfb1799 <nfb1799@rit.edu>
2021-11-19 21:47:57 -0500

    complete

--- server/test/app.js
+++ server/test/app.js
@@ -1,1 +1,12 @@
 require('./test_config.js');
+require('chai').should(); // should is a specific syntax for chai
+
+const app = require('../src/app.js');
+
+describe('server', () => {
+  it('should start up the server', (done) => {
+    app.should.not.be.an('undefined'); // lol chai syntax
+
+    done();
+  });
+});
